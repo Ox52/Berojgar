e44a3dfb81622c1ee93b4fbee5797498c8483603
Ox52 <harsh04241@gmail.com>
2026-03-17 21:51:06 +0530

    /..

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/auth/auth";
 
+export default async function proxy(request: NextRequest) {
+  if (request.method === "OPTIONS") {
+    return new NextResponse(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
 
+  const session = await getSession();
 
-export default async function proxy(request:NextRequest){
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
 
-    const session = await getSession();
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
-
-    const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in")
-    const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up")
-
-    if((isSignInPage ||isSignUpPage ) && session?.user){
-        return NextResponse.redirect( new URL("/dashboard", request.url));
-    }
-
-
-    return NextResponse.next();
-
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
